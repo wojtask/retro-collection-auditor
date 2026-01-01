0359a1d4439c41eb05d0093feb9e8729204542e2
Krzysztof Wojtas <kwojtas@gmail.com>
2026-01-01 15:34:17 +0100

    Delete index.tsx

--- index.tsx
+++
@@ -1,4 +0,0 @@
-// This file is deprecated.
-// Please use `node scan.js` for the CLI functionality.
-// See index.html for instructions.
-console.log("RetroCollection Auditor is now a CLI tool. Please run 'node scan.js'.");
